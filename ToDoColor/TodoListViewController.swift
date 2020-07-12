2666993c4e52d49b83e71e278d01c4d7bd7219fa
Mehmet Atakan Çavuşlu <mcavuslu15@ku.edu.tr>
2020-07-13 01:38:14 +0300

    Add ToDoListViewController with predefined item list.

---
+++ ToDoColor/TodoListViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ToDoColor
+//
+//  Created by Atakan Çavuşlu on 13.07.2020.
+//  Copyright © 2020 Atakan Çavuşlu. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - TableViewDataSource methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+        
+    }
+    
+}
+
