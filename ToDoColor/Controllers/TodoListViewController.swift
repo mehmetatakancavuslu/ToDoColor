40df318af1d99ccb93c5b486092a76ace0662205
Mehmet Atakan Çavuşlu <mcavuslu15@ku.edu.tr>
2020-07-16 16:16:41 +0300

    Implement searchBar search functionality.

--- ToDoColor/Controllers/TodoListViewController.swift
+++ ToDoColor/Controllers/TodoListViewController.swift
@@ -96,15 +96,31 @@ class TodoListViewController: UITableViewController {
         
     }
     
-    func loadItems() {
-        
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
         
         do {
             itemArray = try context.fetch(request)
         } catch {
             print("Error fetching data from context, \(error)")
         }
+        
+    }
+    
+}
+
+// MARK: - UISearchBarDelegate methods
+
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+        
+        tableView.reloadData()
         
     }
     
