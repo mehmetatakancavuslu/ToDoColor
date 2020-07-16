eacd188d3ba73eecd389e00e242141f02d6918ef
Mehmet Atakan Çavuşlu <mcavuslu15@ku.edu.tr>
2020-07-16 16:28:03 +0300

    Finalize searchBar functionality.

--- ToDoColor/Controllers/TodoListViewController.swift
+++ ToDoColor/Controllers/TodoListViewController.swift
@@ -14,14 +14,14 @@ class TodoListViewController: UITableViewController {
     var itemArray = [Item]()
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadItems()
         
     }
-
+    
     // MARK: - TableViewDataSource methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -104,6 +104,8 @@ class TodoListViewController: UITableViewController {
             print("Error fetching data from context, \(error)")
         }
         
+        tableView.reloadData()
+        
     }
     
 }
@@ -112,15 +114,26 @@ class TodoListViewController: UITableViewController {
 
 extension TodoListViewController: UISearchBarDelegate {
     
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
-        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
-        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-        
-        loadItems(with: request)
-        
-        tableView.reloadData()
+        if searchBar.text?.count == 0 {
+            
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+                
+            }
+            
+        } else {
+            
+            let request: NSFetchRequest<Item> = Item.fetchRequest()
+            request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+            request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+            
+            loadItems(with: request)
+            
+        }
         
     }
     
